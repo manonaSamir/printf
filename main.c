8d41494c99f27b10268066f4459e168ea2dbf1c7
manonaSamir <bena.yalla@yahoo.com>
2023-10-18 15:35:55 +0300

    3task

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ int main(void)
 {
 	int len, len2;
 
-	len = _printf("%x", 0);
-	len2 = printf("%x", 0);
+	len = _printf("%S", "No special character.");
+	len2 = printf("No special character.");
 	fflush(stdout);
 	if (len != len2)
 	{
